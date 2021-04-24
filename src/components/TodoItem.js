bcac2c75807cc1c478985c0df93eda5ee446dfa0
Farid Eyvazov <ferideyvazov25@gmail.com>
2021-04-24 14:33:38 +0400

    redux-counter

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoItem = ({name}) => {
+    return (
+        <div>
+            <h1 className="title">Welcome</h1>
+        </div>
+    )
+}
+
+export default TodoItem
